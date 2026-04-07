c187ad164e6faea1542e6082cc130d296081e82d
tomit4 <mosssap@gmail.com>
2026-04-06 22:36:39 -0700

    :memo: Added missing comment

--- list_avg_price_of_products_per_category.sql
+++ list_avg_price_of_products_per_category.sql
@@ -1,5 +1,11 @@
 USE electronics_retail;
 
+-- Purpose: Displays the average price of products per category.
+-- SELECT specifies the fields to be returned: category name and average price.
+-- CAST is used to format the average price to two decimal places for readability.
+-- FROM specifies the table to query (products as alias p).
+-- JOIN joins the products table with the categories table wherever the category IDs match.
+-- GROUP BY groups the results by category name so that the AVG function calculates the average price per category.
 SELECT
   c.category_name,
   CAST(AVG(p.unit_price) AS DECIMAL(10, 2)) AS avg_price
